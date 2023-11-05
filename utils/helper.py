7409ee488bfc5272715a1aa0be6065d39ff4b5b9
icebear <969549808@qq.com>
2023-11-05 21:21:11 +0800

    📃 docs(utils.helper): type hint
    add type hint

--- utils/helper.py
+++ utils/helper.py
@@ -3,7 +3,7 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 
 
-def user_agent_headers():
+def user_agent_headers() -> dict[str, str]:
     return {
         "User-Agent": ua.random,
     }
