24b69eb0e64ea986638a357de356c1d452277be0
icebear <969549808@qq.com>
2023-11-26 17:45:54 +0800

    feat(settings): .env added
    Add .env to manage settings variables

--- conf/settings.py
+++ conf/settings.py
@@ -1,8 +1,12 @@
 import logging
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
+DEBUG = env.bool("DEBUG", False)
 
-LOG_LEVEL = logging.DEBUG if DEBUG else logging.INFO
+LOG_LEVEL = env.int("LOG_LEVEL", logging.DEBUG if DEBUG else logging.INFO)
