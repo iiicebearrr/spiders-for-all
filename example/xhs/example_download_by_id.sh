0127dcea0b687554c76b80e491469d3904cb1a2d
iiicebearrr <110714291+iiicebearrr@users.noreply.github.com>
2024-01-12 12:21:15 +0800

    ✨ feat(xhs): cli
    new features on xhs cli entry. Optimize some behavior of download-by-author

--- example/xhs/example_download_by_id.sh
+++ example/xhs/example_download_by_id.sh
@@ -1,3 +1,6 @@
 # Download note(s) by note id(s)
 
-python -m spiders_for_all xhs download -i 659b671600000000110305b8,6598fac8000000001a0296a3 -s /tmp/xhs_download_by_id
+python -m spiders_for_all xhs download-by-id -i 659b671600000000110305b8,6598fac8000000001a0296a3 -s /tmp/xhs_download_by_id
+
+# Download note(s) by sql
+python -m spiders_for_all xhs download-by-sql "select note_id from t_xhs_author_notes limit 5" -s /tmp/xhs_download_by_sql
