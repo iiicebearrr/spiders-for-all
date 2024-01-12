d09f1cf24dc454ff700cc806cc927b32204abdf2
iiicebearrr <110714291+iiicebearrr@users.noreply.github.com>
2024-01-12 11:26:03 +0800

    ✨ feat(bilibili.cli): cli optimize
    optimize the behavior of download-by-author, add download-by-sql entry

--- example/bilibili/example_download_video.sh
+++ example/bilibili/example_download_video.sh
@@ -1,5 +1,8 @@
 # download by bvid
-python -m spiders_for_all bilibili download-video -b BV1xJ411x7fd -s /tmp/bilibili_download_video
+python -m spiders_for_all bilibili download-by-id -b BV1xJ411x7fd -s /tmp/bilibili_download_video
 
 # multiple download
-python -m spiders_for_all bilibili download-videos -b BV1HM411h7YB,BV1rG4y1j7TJ -s /tmp/bilibili_download_videos
+python -m spiders_for_all bilibili download-by-ids -b BV1HM411h7YB,BV1rG4y1j7TJ -s /tmp/bilibili_download_videos
+
+# download by sql
+python -m spiders_for_all bilibili download-by-sql "select bvid from t_bilibili_author_video limit 5" -s /tmp/bilibili_download_videos_by_sql
