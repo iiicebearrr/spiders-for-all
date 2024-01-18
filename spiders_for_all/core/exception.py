701eab5916978b3e9bef1f84701dc7a64942e730
iiicebearrr <110714291+iiicebearrr@users.noreply.github.com>
2024-01-18 12:30:12 +0800

    🎈 perf(downloader): retry optimize
    try backup url if connection error occured for base_url when itering the content

--- spiders_for_all/core/exception.py
+++ spiders_for_all/core/exception.py
@@ -2,3 +2,8 @@ class MaxRetryExceedError(Exception):
     def __init__(self, retries: int) -> None:
         msg = f"Max retries {retries} has been exceeded"
         super().__init__(msg)
+
+
+class ReWriteRequiredError(Exception):
+    def __init__(self, msg: str) -> None:
+        super().__init__(msg)
